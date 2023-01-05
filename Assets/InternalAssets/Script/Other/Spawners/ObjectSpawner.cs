a24ed33882a494725b5880266f6bcf25410a37b3
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-01-05 20:06:59 +0300

    Разделил ассеты на Internal&External +импорт UniTask

---
+++ Assets/InternalAssets/Script/Other/Spawners/ObjectSpawner.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private int spawnTimer = 6000;
+
+    [SerializeField] private Vector2 minimum;
+    [SerializeField] private Vector2 maximum;
+
+    [SerializeField] private GameObject[] objects;
+
+    private void Start() => Spawning();
+
+    private async void Spawning()
+    {
+        while (true)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Vector2 position = new Vector2(Random.Range(minimum.x, maximum.x), Random.Range(minimum.y, maximum.y));
+                Instantiate(objects[i], position, Quaternion.identity);
+            }
+            await Task.Delay(spawnTimer);
+        }
+    }
+}
