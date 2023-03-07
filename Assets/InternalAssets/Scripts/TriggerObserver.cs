3447d4179c87c09ad560a40f10dc3f8e74628c49
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-03-07 23:01:58 +0300

    Подредактировал коллайдеры префабов декораций

---
+++ Assets/InternalAssets/Scripts/TriggerObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace IceWasteland
+{
+    public sealed class TriggerObserver : MonoBehaviour
+    {
+        public event Action<Collider2D> OnTriggerEnter;
+        public event Action<Collider2D> OnTriggerExit;
+
+        private void OnTriggerEnter2D(Collider2D collision) => OnTriggerEnter?.Invoke(collision);
+        private void OnTriggerExit2D(Collider2D collision) => OnTriggerExit?.Invoke(collision);
+    }
+}
